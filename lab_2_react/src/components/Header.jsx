8a912b5a388088b36e7dcf3057b3a01cb1c12fda
viktoriaanovka <viktoriaanovka532@gmail.com>
2026-05-04 02:24:03 +0300

    Update Header.jsx

--- lab_2_react/src/components/Header.jsx
+++ lab_2_react/src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header({ theme, onToggleTheme }) {
     <header className="bg-blue-700 text-white text-center p-8">
       <h1 className="text-4xl font-bold mb-2">Яновка Вікторія</h1>
       <p className="text-lg">
-        Студентка Національного університету "Львівська політехніка"
+        Студентка Національного иомоуніверситету "Львівська політехніка"
       </p>
       <p className="text-md mt-2 opacity-90">Спеціальність: Кібербезпека</p>
 

766cce7024e1b1a4fc9671076e45aee3b10d1198
viktoriaanovka <viktoriaanovka532@gmail.com>
2026-05-04 02:22:45 +0300

    Update Languages.jsx

--- lab_2_react/src/components/Languages.jsx
+++ lab_2_react/src/components/Languages.jsx
@@ -3,7 +3,7 @@ function Languages() {
     <section className="bg-slate-50 p-5 rounded-xl shadow hover:shadow-lg transition">
       <h2 className="text-2xl font-semibold text-blue-700 mb-3">Мови</h2>
       <ul className="list-disc pl-6 text-slate-700 space-y-2">
-        <li>укр</li>
+        <li>Українська</li>
         <li>Англійська</li>
       </ul>
     </section>
